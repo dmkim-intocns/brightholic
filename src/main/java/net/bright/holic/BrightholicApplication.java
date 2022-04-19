26caf877fd520b346c5106048e592a99412797f1
dmkim-intocns <dmkim@intocns.com>
2022-04-19 15:15:29 +0900

    Initial commit

---
+++ src/main/java/net/bright/holic/BrightholicApplication.java
@@ -0,0 +1,13 @@
+package net.bright.holic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrightholicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrightholicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/bright/holic/BrightholicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bright.holic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrightholicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
